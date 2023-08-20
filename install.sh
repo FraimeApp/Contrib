065dad2424f3b6925e7a774a3412cee5240427ef
Kirill Feschenko <swipduces@yandex.com>
2023-08-19 20:47:19 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd /workspace
 
 apt-get update && apt-get install -y git
 
-git clone https://feschenko:${GITHUB_TOKEN}@github.com/FraimeApp/ProcessingService
+git clone https://${GITHUB_TOKEN}@github.com/FraimeApp/ProcessingService.git
 
 cd /workspace/ProcessingService
 
