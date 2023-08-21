e7b772565f5cd12228f4f87390d2f15821ac3ab1
Kirill Feschenko <swipduces@yandex.com>
2023-08-20 20:20:36 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-cd /workspace
-
-apt-get update && apt-get install -y git
-
-git clone https://${GITHUB_TOKEN}@github.com/FraimeApp/ProcessingService.git
-
 cd /workspace/ProcessingService
 
-apt-get install -y python3.10-venv python3.10-dev
+apt-get update && apt-get install -y python3.10-venv python3.10-dev
 
 python3.10 -m venv venv
 
 source venv/bin/activate
 
 venv/bin/pip install -r requirements.txt
+
+venv/bin/python3.10 run_processing.py
